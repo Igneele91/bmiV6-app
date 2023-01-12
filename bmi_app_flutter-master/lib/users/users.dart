813922cc73f6d2d101b36fa4787e681c0f12e119
Igneele <69846931+Igneele91@users.noreply.github.com>
2023-01-13 00:09:26 +0100

    name and user file point 0

---
+++ bmi_app_flutter-master/lib/users/users.dart
@@ -0,0 +1,8 @@
+class usersFormular extends StatelessWidget {
+  const usersFormular({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
